7345bef1fd3bc7e94a13c2b5f6fdface8af8c994
sharvin <sharvin.bendre@gmail.com>
2021-04-02 23:51:59 +1100

    uploading exercise 6 palindrome numbers and added coments for exercise 6

--- python_practice/exercise_5_list_intersection.py
+++ python_practice/exercise_5_list_intersection.py
@@ -1,3 +1,11 @@
+#Take two lists and write a program that returns a list that contains only 
+#the elements that are common between the lists(without duplicates). 
+#Make sure your program works on two lists of different sizes.
+
+# Extras:
+
+#Randomly generate two lists to test this
+#Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
 import random
 L1=[2,3,5,6,7,8,9,0,"JRE"]
 L2=[1,3,5,6,7.99,4.4,9,0,"LFP"]
